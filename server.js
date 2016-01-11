58f7adb2effe7cd288b59e648a7a6dc39f482edc
Nicholas Stees <nstees@gmail.com>
2016-01-10 22:05:21 -0500

    Added Underscorejs functions

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -19,14 +21,8 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo; 
-	//iterate of array for match
-
-	todos.forEach(function (todo) {
-		if(todo.id === todoId){
-			matchedTodo = todo;
-		}
-	});
+	//use underscore.js to find match
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if(matchedTodo){
 		res.json(matchedTodo);
@@ -39,9 +35,14 @@ app.get('/todos/:id', function (req, res) {
 
 //POST /todos/
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
 
 	body.id = todoNextId++;
+	body.description = body.description.trim();
 	todos.push(body);
 
 	res.json(body);
